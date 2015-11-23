74d4f24c879e99b8ae4ddde977af3dd2ea747738
crhistophersepulveda <crhistopher.sepulveda@live.cl>
2015-11-23 14:22:36 -0300

    direccionamiento

--- comercial_byc/application/config/config.php
+++ comercial_byc/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/comercial_byc';
+$config['base_url'] = 'http://localhost/Comercial_web/comercial_byc';
 
 /*
 |--------------------------------------------------------------------------
